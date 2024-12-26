116c17ffb32d92118a9b438e92623c6ae7f79672
Ctest704 <cgithub9@gmail.com>
2024-12-26 12:02:50 -0300

    adaquando os textos as boas práticas

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'Jogo do Número Secreto';
+//let titulo = document.querySelector('h1');
+//titulo.innerHTML = 'Jogo do Número Secreto';
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+//let paragrafo = document.querySelector('p');
+//paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector();
+    campo.innerHTML = texto;
+}
+
+exibirTextoNaTela('h1', 'Jogo do Número Secreto');
+exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
 
 function verificarChute() {
-    console.log('clicou')
+    console.log('clicou');
 }
