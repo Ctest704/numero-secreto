5a1f868a9dfc0a4f39db04d1db33b0c6e7da79ed
Ctest704 <cgithub9@gmail.com>
2025-01-15 08:52:43 -0300

    alterando chamadas dos textos

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
 //let paragrafo = document.querySelector('p');
 //paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
 let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
 
 function exibirTextoNaTela(tag, texto) {
     let campo = document.querySelector();
@@ -17,16 +18,19 @@ function verificarChute() {
     let chute = document.querySelector('input').value;
     if (chute == numeroSecreto) {
         exibirTextoNaTela('h1', 'Acertou');
-        exibirTextoNaTela('p', 'Você descobriu o número secreto!');
+        let palavraTEntativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTEntativa}!`
+        exibirTextoNaTela('p', mensagemTentativas);
     } else {
         if (chute > numeroSecreto) {
             exibirTextoNaTela('p', 'O número secreto é menor');
         } else {
             exibirTextoNaTela('p', 'O número secreto é maior');
         }
+        tentativas++;
     }
 }
 
 function gerarNumeroAleatorio() {
     return parseInt(Math.random() * 10 + 1);
-}
+}1
